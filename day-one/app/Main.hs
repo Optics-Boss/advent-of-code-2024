909d636149bc1ab9c1d983283d713ae682bd5939
Optics-Boss <->
2024-12-01 11:58:41 +0100

    init

---
+++ day-one/app/Main.hs
@@ -0,0 +1,11 @@
+module Main where
+import Data.List
+
+main :: IO ()
+main = do
+  putStrLn "Hello, Haskell!"
+  let emptyList = [1,2,3,4,5,6]
+  -- let sortedList = sort.emptyList
+  print sortedList
+
+
