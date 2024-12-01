b3f2e128a4fbc538839f2177ed2f8e7a18e404fc
Optics-Boss <->
2024-12-01 16:23:32 +0100

    Tried with haskell and failed

--- day-one/app/Main.hs
+++ day-one/app/Main.hs
@@ -1,11 +1,12 @@
 module Main where
-import Data.List
+
+parseToTwoArrays :: String -> (Int, Int)
+parseToTwoArrays line = (number1, number2)
+  where (read number1, read number2) -> break (== ' ') line
 
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
-  let emptyList = [1,2,3,4,5,6]
+  print $ parseToTwoArrays . lines <$> readFile "input.txt"
+  -- let emptyList = [1,2,3,4,5,6]
   -- let sortedList = sort.emptyList
-  print sortedList
-
-
+  -- print sortedList
