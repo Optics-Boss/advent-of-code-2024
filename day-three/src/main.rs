edfffa29ee5ec12f8101c20ecb97dfb8057f0ca5
Optics-Boss <->
2024-12-03 21:35:32 +0100

    Finished day three one

---
+++ day-three/src/main.rs
@@ -0,0 +1,42 @@
+use std::io;
+use std::fs::File;
+use std::io::BufRead;
+use std::path::Path;
+use regex::Regex;
+
+fn main() {
+    if let Ok(lines) = read_lines("input.txt") {
+        let mut result = 0;
+        for line in lines.flatten() {
+            let re = Regex::new("mul[(]\\d*[,]\\d*[)]").unwrap();
+            let caps : Vec<&str> = re.find_iter(&line)
+                .map(|hay| hay.as_str())
+                .collect(); 
+
+            for test in caps {
+                let mut testing = test.to_string();
+                testing.pop();
+
+                for _i in 0..4 {
+                    testing.remove(0);
+                }
+
+                let numbers : Vec<i32> = testing.split(",")
+                    .filter_map(|f| f.parse::<i32>().ok())
+                    .collect();
+
+                let final_number = numbers.get(0).unwrap_or(&0) * numbers.get(1).unwrap_or(&0);
+                result += final_number
+            }
+        }
+
+        println!("Result = {:?}", result);
+    }
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
